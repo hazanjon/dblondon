76e46314282290c7f40a509f37a441312d2f2d53
Jon Hazan <jon@hazan.me>
2015-01-24 09:02:38 +0000

    More 3d testing

--- public/javascripts/myo_interface.js
+++ public/javascripts/myo_interface.js
@@ -1,6 +1,13 @@
 var myMyo = Myo.create();
+myMyo.orientationOffset = myMyo._lastQuant; //Zero the myo
+
 myMyo.on('fingers_spread', function(edge){
     if(!edge) return;
-    alert('Hello Myo!');
+    console.log('fingers_spread Myo!');
     myMyo.vibrate();
+});
+
+myMyo.on('orientation', function(data){
+    if(!data) return;
+    console.log((Math.round(data.x*100)/100) + " " + (Math.round(data.y*100)/100) + " " + (Math.round(data.z*100)/100));
 });
--- public/javascripts/three_interface.js
+++ public/javascripts/three_interface.js
@@ -12,9 +12,30 @@ scene.add( cube );
 
 camera.position.z = 5;
 
+controls = new THREE.OrbitControls( camera );
+controls.damping = 0.2;
+controls.addEventListener( 'change', render );
+
+var y_rotatespeed = 0;
+var x_rotatespeed = 0;
+
+myMyo.on('orientation', function(data){
+    if(!data) return;
+
+	y_rotatespeed = 0;
+    if(Math.abs(data.z) > 0.2)
+    	y_rotatespeed = data.z;
+
+	x_rotatespeed = 0;
+    if(Math.abs(data.y) > 0.2)
+    	x_rotatespeed = data.y;
+    //console.log((Math.round(data.x*100)/100) + " " + (Math.round(data.y*100)/100) + " " + (Math.round(data.z*100)/100));
+});
+
 function render() {
 	requestAnimationFrame( render );
-	cube.rotation.y += 0.01;
+	// cube.rotation.y += (y_rotatespeed / 10);
+	// cube.rotation.x -= (x_rotatespeed / 10);
 	renderer.render( scene, camera );
 }
 render();
